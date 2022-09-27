eb842e57ce1a71fc37176f10797c759050390db8
Takashi Mori <kanetugu2015@gmail.com>
2022-09-27 09:25:57 +0900

    renamed

--- env/env.bash
+++ env/env.bash
@@ -2,7 +2,7 @@
 
 export WEB_SERVER_URL=192.168.11.52
 export TOP_DIR=`pwd`
-export TEST_TARGET=exment
+export TEST_TARGET=sample
 export TEST_IMPL_DIR=${TOP_DIR}/test-impl/${TEST_TARGET}
 export TEST_ITEM_DIR=${TOP_DIR}/test-item/${TEST_TARGET}
 export TEST_RNTM_DIR=${TOP_DIR}/test-runtime
