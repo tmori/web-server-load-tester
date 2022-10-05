51a5f58bc3d3aeaca84ea7096adb97d1f348c200
Takashi Mori <kanetugu2015@gmail.com>
2022-10-05 13:44:18 +0900

    add args for setup

--- test-runtime/controller/setup.bash
+++ test-runtime/controller/setup.bash
@@ -11,10 +11,13 @@ then
     terror "Usage: $0 <impl> <TestNo>"
     exit 1
 fi
+IMPL_ORG=${1}
 
-IMPL=${1}
+IMPL=`echo ${IMPL_ORG} | awk -F\( '{print $1}'`
+IMPL_ARGS=`echo ${IMPL_ORG} | awk -F\( '{print $2}' | awk -F\) '{print $1}' | sed -s 's/,/ /g'`
+
 TEST_NO=${2}
 #tlog "START SETUP"
-bash ${TEST_IMPL_DIR}/controller/setup/${IMPL}-setup.bash ${TEST_NO}
+bash ${TEST_IMPL_DIR}/controller/setup/${IMPL}-setup.bash ${TEST_NO} ${IMPL_ARGS}
 #tlog "END SETUP"
 
